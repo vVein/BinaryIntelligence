b6eb3b01d32719f56dddaae3e70ce332e66e1b5f
Talon <Talon99@outlook.com>
2021-09-11 00:44:11 -0400

    p1

--- Main.py
+++ Main.py
@@ -10,15 +10,21 @@ def starting_centre_point (img_array):
       first_x = int(np.size(img_array,1)/2)
       return first_x, first_y
 
-def colour_inspection_shape (size, img_array, centre_x, centre_y ):
+def colour_inspection_shape (size, img_array, centre_x_1, centre_y_1 ):
      # rows, cols = (1, 2)
      # inspection_colours = [[0 for i in range(cols)] for j in range(rows)]
+     # print(centre_x_1, centre_y_1)
       inspection_colours = []
       for i in range(-size,size+1):
             for j in range (-(size-abs(i)),size+1-abs(i)):
                  # print(i,j)
-                  x_ordinate = centre_x + j
-                  y_ordinate = centre_y + i
+                  x_ordinate = centre_x_1 + j
+                  if x_ordinate < 0 or x_ordinate >= img_width:
+                        continue
+                  y_ordinate = centre_y_1 + i
+                  if y_ordinate < 0 or y_ordinate >= img_height:
+                        continue
+                  # print (x_ordinate, y_ordinate)
                   pixel = img_array[y_ordinate][x_ordinate]
                   inspection_colours.append([pixel[0],pixel[1],pixel[2]])
       
@@ -67,9 +73,13 @@ numpydata = asarray(img)
 #print(img.mode)
 
 #print(numpydata[0][0][2])
-first_centre_x = int(np.size(numpydata,1)/2)
-first_centre_y = int(len(numpydata) / 2)
 shape_size = 5
+img_width = int(np.size(numpydata,1))
+img_height = int(len(numpydata))
+
+centre_x = shape_size
+centre_y = shape_size
+
 minimum_match_ratio = 0.7
 edges= []
 
@@ -77,20 +87,39 @@ def circular_sweep (centre_x, centre_y):
       circular_pattern = [[0,2],[1,1],[2,0],[1,-1],[0,-2],[-1,-1],[-2,0],[-1,1]]
       for i in range(len(circular_pattern)):
             next_x = centre_x + shape_size * circular_pattern[i][0]
+            if next_x < 0 or next_x >= img_width:
+                  continue
             next_y = centre_y + shape_size * circular_pattern[i][1]
+            if next_y < 0 or next_y >= img_height:
+                  continue
             pattern_B = colour_inspection_shape(shape_size,numpydata,next_x,next_y)
-            print(next_x)
+            if len(pattern_B[0]) == 0 or len(pattern_A[0]) == 0 :
+                  continue
+           # print(pattern_A,pattern_B)
             match_ratio = pattern_match(pattern_A,pattern_B)
             if match_ratio < minimum_match_ratio:
-                  x_ordinate = min(first_centre_x, next_x) + abs(first_centre_x - next_x)
-                  y_ordinate = min(first_centre_y, next_y) + abs(first_centre_y - next_y)
+                 # print(pattern_A,pattern_B)
+                  x_ordinate = min(centre_x, next_x) + abs(centre_x - next_x)
+                  y_ordinate = min(centre_y, next_y) + abs(centre_y - next_y)
                   edges.append([x_ordinate,y_ordinate])
 
-pattern_A = colour_inspection_shape(shape_size,numpydata,first_centre_x,first_centre_y)
-print(pattern_A)
-circular_sweep(first_centre_x,first_centre_y)
+pattern_A = colour_inspection_shape(shape_size,numpydata,centre_x,centre_y)
+#print(pattern_A)
+circular_sweep(centre_x,centre_y)
 
+no_x_adjustments = int((img_width / shape_size)-5)
+no_y_adjustments = int((img_height / shape_size)-5)
+
+for i in range(no_y_adjustments):
+      centre_y = centre_y + shape_size
+      centre_x = shape_size
+      for j in range(no_x_adjustments):
+            centre_x = centre_x + shape_size
+            pattern_A = colour_inspection_shape(shape_size,numpydata,centre_x,centre_y)
+            circular_sweep(centre_x,centre_y)
 print(edges)
+plt.scatter(*zip(*edges))
+plt.show()
 
 #img = Image.fromarray(numpydata, 'RGB')
 #img.save('my.png')
---
+++ Test.py
@@ -0,0 +1,28 @@
+from PIL import Image
+from numpy import asarray
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# load the image and convert into
+# numpy array
+img = Image.open(r'C:\Users\Stude\OneDrive\Pictures\Image-Analysis\Shapes.jpg')
+  
+# asarray() class is used to convert
+# PIL images into NumPy arrays
+numpydata = asarray(img)
+  
+# <class 'numpy.ndarray'>
+#print(type(numpydata))
+  
+#  shape
+print(numpydata.shape)
+
+print(img.mode)
+
+print(numpydata[331][547][2])
+
+img = Image.fromarray(numpydata, 'RGB')
+img.save('my.png')
+plt.imshow(img)
+plt.show()
