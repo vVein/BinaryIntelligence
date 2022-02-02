4089b99a3d9ce44f6171c730ceaae126ab5891c2
Talon <Talon99@outlook.com>
2022-02-01 23:28:17 -0500

    v5.0.1 working

--- Edge_processing.py
+++ Edge_processing.py
@@ -7,7 +7,8 @@ plt.rcParams['figure.facecolor'] = 'white'
 
 # Lateral scan reduction (Left ro right)
 def lat_edge_processing(numpydata, edges_lat, singular_RGB_trigger, RGB_tolerance, width_threshold):
-    reduced_edges = []
+    reduced_lat_edges = []
+    remaining_lat_edges = []
     first = True
     skip_next = False
     for xy in edges_lat:
@@ -32,9 +33,9 @@ def lat_edge_processing(numpydata, edges_lat, singular_RGB_trigger, RGB_toleranc
                         mid_xy = [mid_x, current_xy[1]]
                         
                         if width == 1:
-                                reduced_edges.append(mid_xy)
-                                skip_next = True
-                                continue
+                            reduced_lat_edges.append(mid_xy)
+                            skip_next = True
+                            continue
                         
                         if width < width_threshold:
 
@@ -58,33 +59,34 @@ def lat_edge_processing(numpydata, edges_lat, singular_RGB_trigger, RGB_toleranc
                                 different_colour = any([different_colour1, different_colour2])
                             
                             if not different_colour:
-                                reduced_edges.append(mid_xy)
+                                reduced_lat_edges.append(mid_xy)
                                 skip_next = True
                                 continue
                             
                         else:
-                            reduced_edges.append(previous_xy)
+                            remaining_lat_edges.append(previous_xy)
                             continue
 
                     else:
-                        reduced_edges.append(previous_xy)
+                        remaining_lat_edges.append(previous_xy)
                         continue
                 else:
-                    reduced_edges.append(previous_xy)
+                    remaining_lat_edges.append(previous_xy)
             else:
                 skip_next = False
-       
-    x_cords_3, y_cords_3 = zip(*reduced_edges)
-    plt.scatter(*zip(*reduced_edges),marker='.', s=0.1, color='green')
-    plt.scatter(x_cords_3, y_cords_3, marker='.', s=0.5, color='green')
-    plt.gca().invert_yaxis()
-    plt.legend()
-    img = Image.fromarray(numpydata, 'RGB')
-    img.save('my.png')
-    plt.imshow(img)
-    plt.show()
     
-    return reduced_edges
+    if 20 == 2:
+        x_cords_3, y_cords_3 = zip(*reduced_lat_edges)
+        plt.scatter(*zip(*reduced_lat_edges),marker='.', s=0.1, color='green')
+        plt.scatter(x_cords_3, y_cords_3, marker='.', s=0.5, color='green')
+        plt.gca().invert_yaxis()
+        plt.legend()
+        img = Image.fromarray(numpydata, 'RGB')
+        img.save('my.png')
+        plt.imshow(img)
+        plt.show()
+    
+    return reduced_lat_edges, remaining_lat_edges
 
 def vert_edge_processing(numpydata, edges_vert, singular_RGB_trigger, RGB_tolerance, height_threshold):
     reduced_vert_edges = []
@@ -113,9 +115,9 @@ def vert_edge_processing(numpydata, edges_vert, singular_RGB_trigger, RGB_tolera
                         mid_xy = [current_xy[0], mid_y]
                         
                         if height == 1:
-                                reduced_vert_edges.append(mid_xy)
-                                skip_next = True
-                                continue
+                            reduced_vert_edges.append(mid_xy)
+                            skip_next = True
+                            continue
                         
                         if height < height_threshold:
 
@@ -154,21 +156,214 @@ def vert_edge_processing(numpydata, edges_vert, singular_RGB_trigger, RGB_tolera
                     remaining_vert_edges.append(previous_xy)
             else:
                 skip_next = False
-       
-    x_cords_2, y_cords_2 = zip(*reduced_vert_edges)
-    plt.scatter(*zip(*reduced_vert_edges),marker='.', s=0.1, color='green')
-    plt.scatter(x_cords_2, y_cords_2, marker='.', s=0.5, color='green')
-    x_cords_1, y_cords_1 = zip(*remaining_vert_edges)
-    plt.scatter(*zip(*remaining_vert_edges),marker='.', s=0.1, color='blue')
-    plt.scatter(x_cords_1, y_cords_1, marker='.', s=0.5, color='blue')
-    plt.gca().invert_yaxis()
-    plt.legend()
-    img = Image.fromarray(numpydata, 'RGB')
-    img.save('my.png')
-    plt.imshow(img)
-    plt.show()
     
-    return reduced_vert_edges
+    if 20 == 2:
+        x_cords_2, y_cords_2 = zip(*reduced_vert_edges)
+        plt.scatter(*zip(*reduced_vert_edges),marker='.', s=0.1, color='green')
+        plt.scatter(x_cords_2, y_cords_2, marker='.', s=0.5, color='green')
+        x_cords_1, y_cords_1 = zip(*remaining_vert_edges)
+        plt.scatter(*zip(*remaining_vert_edges),marker='.', s=0.1, color='blue')
+        plt.scatter(x_cords_1, y_cords_1, marker='.', s=0.5, color='blue')
+        plt.gca().invert_yaxis()
+        plt.legend()
+        img = Image.fromarray(numpydata, 'RGB')
+        img.save('my.png')
+        plt.imshow(img)
+        plt.show()
+    
+    return reduced_vert_edges, remaining_vert_edges
+
+def diag_LR_edge_processing(numpydata, edges_diag_LR, singular_RGB_trigger, RGB_tolerance, height_threshold):
+    reduced_diag_LR_edges = []
+    remaining_diag_LR_edges = []
+    first = True
+    skip_next = False
+    
+    # Potential flaw here: if order is not preserved then xy will not be served in the correct order and the iterator will either
+    # have to search each diagonal manually for subsequent edges or inline the processing
+    for xy in edges_diag_LR:
+        if first:
+            current_xy = xy
+            first = False
+        else:
+            previous_xy = current_xy
+            current_xy = xy
+            current_x = current_xy[0]
+            current_y = current_xy[1]
+            previous_x = previous_xy[0]
+            previous_y = previous_xy[1]
+            width = int (current_x - previous_x)
+            height = int (current_y - previous_y)
+            
+            if not skip_next:
+                if width == height:
+                    current_pixel = list(numpydata[current_y][current_x])
+                    previous_pixel = list(numpydata[previous_y][previous_x])
+                    different_colour = pixel_comparison_t(current_pixel, previous_pixel, singular_RGB_trigger, RGB_tolerance)
+                    
+                    if not different_colour:
+                        
+                        mid_x = previous_x + width / 2
+                        mid_y = previous_y + height / 2
+                        mid_xy = [mid_x, mid_y]
+                        
+                        if height == 1:
+                            reduced_diag_LR_edges.append(mid_xy)
+                            skip_next = True
+                            continue
+                        
+                        if height < height_threshold:
+
+                            # even height; single middle point
+                            if height % 2 == 0:
+                                mid_x = int( previous_x + width / 2)
+                                mid_y = int( previous_y + height / 2)
+                                mid_xy = [mid_x, mid_y]
+                                middle_pixel = list(numpydata[mid_xy[1]][mid_xy[0]])
+                                different_colour = pixel_comparison_t(current_pixel, middle_pixel, singular_RGB_trigger, RGB_tolerance)
+                                
+                            # odd height; 2 middle points
+                            else:                     
+                                mid_x1 = int( previous_x + 0.5 + width / 2)
+                                mid_x2 = int( previous_x - 0.5 + width / 2)
+                                mid_y1 = int( previous_y + 0.5 + height / 2)
+                                mid_y2 = int( previous_y - 0.5 + height / 2)
+                                mid_xy1 = [mid_x1, mid_y1]
+                                mid_xy2 = [mid_x2, mid_y2]
+                                middle_pixel1 = list(numpydata[mid_xy1[1]][mid_xy1[0]])
+                                middle_pixel2 = list(numpydata[mid_xy2[1]][mid_xy2[0]])
+                                different_colour1 = pixel_comparison_t(current_pixel, middle_pixel1, singular_RGB_trigger, RGB_tolerance)
+                                different_colour2 = pixel_comparison_t(current_pixel, middle_pixel2, singular_RGB_trigger, RGB_tolerance)
+                                different_colour = any([different_colour1, different_colour2])
+                            
+                            if not different_colour:
+                                reduced_diag_LR_edges.append(mid_xy)
+                                skip_next = True
+                                continue
+                            
+                        else:
+                            remaining_diag_LR_edges.append(previous_xy)
+                            continue
+
+                    else:
+                        remaining_diag_LR_edges.append(previous_xy)
+                        continue
+                else:
+                    remaining_diag_LR_edges.append(previous_xy)
+            else:
+                skip_next = False
+    
+    if 20 == 2:
+        x_cords_2, y_cords_2 = zip(*reduced_diag_LR_edges)
+        plt.scatter(*zip(*reduced_diag_LR_edges),marker='.', s=0.1, color='green')
+        plt.scatter(x_cords_2, y_cords_2, marker='.', s=0.5, color='green')
+        x_cords_1, y_cords_1 = zip(*remaining_diag_LR_edges)
+        plt.scatter(*zip(*remaining_diag_LR_edges),marker='.', s=0.1, color='orange')
+        plt.scatter(x_cords_1, y_cords_1, marker='.', s=0.5, color='orange')
+        plt.gca().invert_yaxis()
+        plt.legend()
+        img = Image.fromarray(numpydata, 'RGB')
+        img.save('my.png')
+        plt.imshow(img)
+        plt.show()
+    
+    return reduced_diag_LR_edges, remaining_diag_LR_edges
+
+def diag_RL_edge_processing(numpydata, edges_diag_RL, singular_RGB_trigger, RGB_tolerance, height_threshold):
+    reduced_diag_RL_edges = []
+    remaining_diag_RL_edges = []
+    first = True
+    skip_next = False
+    
+    # Potential flaw here: if order is not preserved then xy will not be served in the correct order and the iterator will either
+    # have to search each diagonal manually for subsequent edges or inline the processing
+    for xy in edges_diag_RL:
+        if first:
+            current_xy = xy
+            first = False
+        else:
+            previous_xy = current_xy
+            current_xy = xy
+            current_x = current_xy[0]
+            current_y = current_xy[1]
+            previous_x = previous_xy[0]
+            previous_y = previous_xy[1]
+            delta_x = int (current_x - previous_x)
+            height = int (current_y - previous_y)
+            
+            if not skip_next:
+                if -1 * delta_x == height:
+                    current_pixel = list(numpydata[current_y][current_x])
+                    previous_pixel = list(numpydata[previous_y][previous_x])
+                    different_colour = pixel_comparison_t(current_pixel, previous_pixel, singular_RGB_trigger, RGB_tolerance)
+                    
+                    if not different_colour:
+                        
+                        mid_x = previous_x + delta_x / 2
+                        mid_y = previous_y + height / 2
+                        mid_xy = [mid_x, mid_y]
+                        
+                        if height == 1:
+                            reduced_diag_RL_edges.append(mid_xy)
+                            skip_next = True
+                            continue
+                        
+                        if height < height_threshold:
+
+                            # even height; single middle point
+                            if height % 2 == 0:
+                                mid_x = int( previous_x + delta_x / 2)
+                                mid_y = int( previous_y + height / 2)
+                                mid_xy = [mid_x, mid_y]
+                                middle_pixel = list(numpydata[mid_xy[1]][mid_xy[0]])
+                                different_colour = pixel_comparison_t(current_pixel, middle_pixel, singular_RGB_trigger, RGB_tolerance)
+                                
+                            # odd height; 2 middle points
+                            else:                     
+                                mid_x1 = int( previous_x + 0.5 + delta_x / 2)
+                                mid_x2 = int( previous_x - 0.5 + delta_x / 2)
+                                mid_y1 = int( previous_y - 0.5 + height / 2)
+                                mid_y2 = int( previous_y + 0.5 + height / 2)
+                                mid_xy1 = [mid_x1, mid_y1]
+                                mid_xy2 = [mid_x2, mid_y2]
+                                middle_pixel1 = list(numpydata[mid_xy1[1]][mid_xy1[0]])
+                                middle_pixel2 = list(numpydata[mid_xy2[1]][mid_xy2[0]])
+                                different_colour1 = pixel_comparison_t(current_pixel, middle_pixel1, singular_RGB_trigger, RGB_tolerance)
+                                different_colour2 = pixel_comparison_t(current_pixel, middle_pixel2, singular_RGB_trigger, RGB_tolerance)
+                                different_colour = any([different_colour1, different_colour2])
+                            
+                            if not different_colour:
+                                reduced_diag_RL_edges.append(mid_xy)
+                                skip_next = True
+                                continue
+                            
+                        else:
+                            remaining_diag_RL_edges.append(previous_xy)
+                            continue
+
+                    else:
+                        remaining_diag_RL_edges.append(previous_xy)
+                        continue
+                else:
+                    remaining_diag_RL_edges.append(previous_xy)
+            else:
+                skip_next = False
+    
+    if 2 == 2:
+        x_cords_2, y_cords_2 = zip(*reduced_diag_RL_edges)
+        plt.scatter(*zip(*reduced_diag_RL_edges),marker='.', s=0.1, color='green')
+        plt.scatter(x_cords_2, y_cords_2, marker='.', s=0.5, color='green')
+        x_cords_1, y_cords_1 = zip(*remaining_diag_RL_edges)
+        plt.scatter(*zip(*remaining_diag_RL_edges),marker='.', s=0.1, color='orange')
+        plt.scatter(x_cords_1, y_cords_1, marker='.', s=0.5, color='orange')
+        plt.gca().invert_yaxis()
+        plt.legend()
+        img = Image.fromarray(numpydata, 'RGB')
+        img.save('my.png')
+        plt.imshow(img)
+        plt.show()
+    
+    return reduced_diag_RL_edges, remaining_diag_RL_edges
     
 def edge_prioritisation(numpydata, edges_lat, edges_vert, edges_diag_LR, edges_diag_RL):
     
@@ -244,8 +439,8 @@ def edge_prioritisation(numpydata, edges_lat, edges_vert, edges_diag_LR, edges_d
         plt.scatter(*zip(*edges_prio_3),marker='.', s=0.1, color='green')
         plt.scatter(x_cords_3, y_cords_3, marker='.', s=0.5, color='green')
         x_cords_2, y_cords_2 = zip(*edges_prio_2)
-        plt.scatter(*zip(*edges_prio_2),marker='.', s=0.1, color='blue')
-        plt.scatter(x_cords_2,y_cords_2,marker='.', s=0.5, color='blue')
+        plt.scatter(*zip(*edges_prio_2),marker='.', s=0.1, color='orange')
+        plt.scatter(x_cords_2,y_cords_2,marker='.', s=0.5, color='orange')
         x_cords, y_cords = zip(*edges_prio_1)
         plt.scatter(*zip(*edges_prio_1),marker='.', s=0.1, color='red')
         plt.scatter(x_cords,y_cords,marker='.', s=0.5, color='red')
